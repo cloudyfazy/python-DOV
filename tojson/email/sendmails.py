e7844ac3f43ff875ecef10be404089499424345c
cloudyfazy <72637315+cloudyfazy@users.noreply.github.com>
2022-11-25 17:13:11 +0100

    Sending automatic mails

---
+++ tojson/email/sendmails.py
@@ -0,0 +1,137 @@
+from email.message import EmailMessage
+import ssl
+import smtplib
+
+email_sender = 'ufedofaith095@gmail.com'
+email_password = 'egmvxtqxzuzkcjyg'
+
+email_receiver= 'faizyedu81@gmail.com'
+
+subject ='HELLO FRIEND'
+body ="""
+How do you do my friend? """
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject'] = subject
+em.set_content(body)
+
+context = ssl.create_default_content()
+
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=ssl.create_default_content() ) as smtp:
+   smtp.login(email_sender,email_password)
+   smtp.sendmail (email_sender, email_receiver, em.as_string())
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+  
