b9403a3b8307230cf48d9689910caf927563f8ee
cloudyfazy <72637315+cloudyfazy@users.noreply.github.com>
2022-12-07 00:26:39 +0100

    Sending mails

--- SENDING MAILS/sendmails.py
+++ SENDING MAILS/sendmails.py
@@ -1,16 +1,22 @@
 from email.message import EmailMessage
 import ssl
 import smtplib
+import json
 
 email_sender = 'ufedofaith095@gmail.com'
 email_password = 'egmvxtqxzuzkcjyg'
-
-email_receiver= 'Faizyedu81@gmail.com'
-
-
+email_receiver =[]
+with open(r"C:\Users\dvrk\Desktop\python DOV\SENDING MAILS\emailsss.json", "r") as file:
+    data = json.load(file)
+    for name, mail in data.items():
+        receiver = mail
+           
+        email_receiver.append(receiver)
+        print(email_receiver)
+    
+        
 subject ='HELLO FRIEND'
-body ="""
-How do you do my friend? """
+body ="""How do you do my friend? """
 
 em = EmailMessage()
 em['From'] = email_sender
@@ -21,8 +27,8 @@ em.set_content(body)
 context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context ) as smtp:
-   smtp.login(email_sender,email_password)
-   smtp.sendmail (email_sender, email_receiver, em.as_string())
+    smtp.login(email_sender,email_password)
+    smtp.sendmail (email_sender, email_receiver, em.as_string())
 
 
 
